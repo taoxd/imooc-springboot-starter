aea895d62ef58e34edb42486b30d398aa2aa54b2
taoxd <250567492@qq.com>
2019-04-16 15:07:00 +0800

    springboot整合拦截器

---
+++ imooc-springboot-starter/src/main/java/com/imooc/config/WebMvcConfigurer.java
@@ -0,0 +1,24 @@
+package com.imooc.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import com.imooc.controller.interceptor.OneInterceptor;
+import com.imooc.controller.interceptor.TwoInterceptor;
+
+@Configuration
+public class WebMvcConfigurer extends WebMvcConfigurerAdapter {
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		/**
+		 * 拦截器按顺序执行
+		 * 注册拦截器和拦截地址
+		 */
+		registry.addInterceptor(new OneInterceptor()).addPathPatterns("/one/**");
+		registry.addInterceptor(new TwoInterceptor()).addPathPatterns("/two/**").addPathPatterns("/one/**");
+		super.addInterceptors(registry);
+	}
+
+}
